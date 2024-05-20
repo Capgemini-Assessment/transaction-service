9347d0fbe18d6c97adca2d032be10b9881faba7e
SachinGitAcc <sachindattatray.mengade@ingka.com>
2024-05-20 20:41:15 +0200

    Initial Commit

---
+++ src/test/java/org/capgemini/blue_harvest/transactionservice/TransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.capgemini.blue_harvest.transactionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
